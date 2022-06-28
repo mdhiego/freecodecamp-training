fa28b71c262f90668227aaa194199c1d10264701
Dhiego Martins Andrade <mdhiego@users.noreply.github.com>
2022-06-28 20:16:25 -0300

    Fix the code of symmetric-difference challenge

--- coding-interview-prep/algorithms/symmetric-difference.js
+++ coding-interview-prep/algorithms/symmetric-difference.js
@@ -1,16 +1,13 @@
-function sym(array1, array2, rest) {
+function sym(...args) {
+    const [array1, array2, ...rest] = args;
     let result = [];
     const [set1, set2] = [new Set(array1), new Set(array2)];
   
-    for (let item of set1) {
-      if (!set2.has(item)) result.push(item);
-    }
-    for (let item of set2) {
-      if (!set1.has(item)) result.push(item);
-    }
-  
-    return (rest ? sym(result, rest) : result).sort();
+    set1.forEach(item => { if (!set2.has(item)) result.push(item); });
+    set2.forEach(item => { if (!set1.has(item)) result.push(item); });
+    
+    return (rest.length === 0 ? result : sym(result, ...rest)).sort();
   }
   
-  console.log(sym([1, 2, 5], [2, 3, 5], [3, 4, 5]));
+  console.log(sym([3, 3, 3, 2, 5], [2, 1, 5, 7], [3, 4, 6, 6], [1, 2, 3]));
   
