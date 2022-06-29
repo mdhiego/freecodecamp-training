4e40018b6d2421ad0ef460030315f6b81dad6984
Dhiego Martins Andrade <mdhiego@users.noreply.github.com>
2022-06-28 22:39:17 -0300

    Add no-repeats-please submission

---
+++ coding-interview-prep/algorithms/no-repeats-please.js
@@ -0,0 +1,31 @@
+function getPermutations(str) {
+    let permutations = [];
+
+    for (let char of str) {
+        if (permutations.length === 0) {
+            permutations.push(char);
+            continue;
+        }
+
+        let newPermutations = [];
+        for (let permutation of permutations) {
+            for (let i = 0; i <= permutation.length; i++) {
+                newPermutations.push(permutation.slice(0, i) + char + permutation.slice(i));
+            }
+        }
+
+        permutations = newPermutations;
+    }
+
+    return permutations;
+}
+
+function permAlone(str) {
+    const permutations = getPermutations(str);
+
+    return permutations.reduce((acc, curr) => {
+        return !curr.match(/([a-z])\1/ig) ? acc + 1 : acc;
+    }, 0);
+}
+
+console.log(permAlone('aabb'));
