726d183fd6cb86a2a29ba92e71f3db9a7a79a64d
Dhiego Martins Andrade <mdhiego@users.noreply.github.com>
2022-06-28 21:15:43 -0300

    Format the code

--- coding-interview-prep/algorithms/symmetric-difference.js
+++ coding-interview-prep/algorithms/symmetric-difference.js
@@ -1,13 +1,16 @@
 function sym(...args) {
-    const [array1, array2, ...rest] = args;
-    let result = [];
-    const [set1, set2] = [new Set(array1), new Set(array2)];
-  
-    set1.forEach(item => { if (!set2.has(item)) result.push(item); });
-    set2.forEach(item => { if (!set1.has(item)) result.push(item); });
-    
-    return (rest.length === 0 ? result : sym(result, ...rest)).sort();
-  }
-  
-  console.log(sym([3, 3, 3, 2, 5], [2, 1, 5, 7], [3, 4, 6, 6], [1, 2, 3]));
-  
+  const [array1, array2, ...rest] = args;
+  let result = [];
+  const [set1, set2] = [new Set(array1), new Set(array2)];
+
+  set1.forEach((item) => {
+    if (!set2.has(item)) result.push(item);
+  });
+  set2.forEach((item) => {
+    if (!set1.has(item)) result.push(item);
+  });
+
+  return (rest.length === 0 ? result : sym(result, ...rest)).sort();
+}
+
+console.log(sym([3, 3, 3, 2, 5], [2, 1, 5, 7], [3, 4, 6, 6], [1, 2, 3]));
