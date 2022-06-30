866cfc79f5b506aa741c765d29ad95f2b7d06bed
Dhiego Martins Andrade <mdhiego@users.noreply.github.com>
2022-06-29 22:03:55 -0300

    Add implement-insertion-sort submission

---
+++ coding-interview-prep/algorithms/implement-insertion-sort.js
@@ -0,0 +1,15 @@
+function insertionSort(array) {
+    for (let i = 1; i < array.length; i++) {
+        let j = i;
+        while (j > 0 && array[j] < array[j - 1]) {
+            let temp = array[j];
+            array[j] = array[j - 1];
+            array[j - 1] = temp;
+            j--;
+        }
+    }
+
+    return array;
+}
+
+console.log(insertionSort([3, 2, 1]));
