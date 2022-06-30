28790ac87fa54fcd9432706ea4821bbc3a06a257
Dhiego Martins Andrade <mdhiego@users.noreply.github.com>
2022-06-29 22:00:35 -0300

    Add implement-bubble-sort submission

--- coding-interview-prep/algorithms/implement-bubble-sort.js
+++ coding-interview-prep/algorithms/implement-bubble-sort.js
@@ -1,8 +1,16 @@
 function bubbleSort(array) {
-    // Only change code below this line
+    
+    for (let i = 0; i < array.length; i++) {
+        for (let j = 0; j < array.length; j++) {
+            if (array[j] > array[j + 1]) {
+                let temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+            }
+        }
+    }
+
     return array;
-    // Only change code above this line
   }
 
   console.log(bubbleSort([3, 2, 1]));
-  
